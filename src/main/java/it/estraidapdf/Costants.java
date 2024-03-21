660d0e2b35d0b025b80c5adcee876c2c93ac0886
Michele Zanolin <michele.zanolin@gmail.com>
2024-03-21 21:25:35 +0100

    Add files via upload

---
+++ src/main/java/it/estraidapdf/Costants.java
@@ -0,0 +1,10 @@
+package it.estraidapdf;
+
+public class Costants {
+	
+	public static String PUNTO = ".";
+	public static final String EMPTY_STRING = "";
+	public static final String SLASH = "/";
+	public static final String UNDERSCORE = "_";
+
+}
