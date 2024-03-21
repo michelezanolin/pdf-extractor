dee0384420c471841d15946ca03b6dadcdc33c50
Michele Zanolin <michele.zanolin@gmail.com>
2024-03-21 22:04:57 +0100

    Add files via upload

---
+++ src/main/java/it/estraidapdf/EstraiTesto.java
@@ -0,0 +1,52 @@
+package it.estraidapdf;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfDocumentContentParser;
+import com.itextpdf.kernel.pdf.canvas.parser.listener.SimpleTextExtractionStrategy;
+
+public class EstraiTesto {
+
+	public static void estraiTestoPdf(String sorgente, String destinazione) throws IOException {
+		String contenuto = estraiContenutoTestualePdf(sorgente);
+		stringToFile(contenuto, destinazione);
+	}
+
+	/**
+	 * metodo per scrivere una stringa in un file
+	 * 
+	 * @param contenuto
+	 * @param destinazione
+	 * @throws IOException
+	 */
+	public static void stringToFile(String contenuto, String destinazione) throws IOException {
+		BufferedWriter out = new BufferedWriter(new FileWriter(destinazione));
+		out.write(contenuto);
+		out.close();
+	}
+
+	/**
+	 * metodo per estrapolare il testo contenuto in un pdf
+	 * 
+	 * @param sorgente
+	 * @return
+	 * @throws IOException
+	 */
+	public static String estraiContenutoTestualePdf(String sorgente) throws IOException {
+		PdfReader pdfReader = new PdfReader(sorgente);
+		PdfDocument pdf = new PdfDocument(pdfReader);
+		PdfDocument pdfDocument = new PdfDocument(new PdfReader(sorgente));
+		PdfDocumentContentParser parser = new PdfDocumentContentParser(pdfDocument);
+		StringBuilder sb = new StringBuilder();
+		for (int pageNumber = 1; pageNumber <= pdf.getNumberOfPages(); pageNumber++) {
+			SimpleTextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+			sb.append(parser.processContent(pageNumber, strategy).getResultantText());
+		}
+		pdf.close();
+		return sb.toString();
+	}
+}
---
+++ src/main/java/it/estraidapdf/Main.java
@@ -0,0 +1,8 @@
+package it.estraidapdf;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		FileChooser.run(new FileChooser(), 250, 110);
+	}
+}
